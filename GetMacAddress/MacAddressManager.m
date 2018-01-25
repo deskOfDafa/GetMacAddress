00daa6e33e8c1c4b57e9c275adbed4b0f1dd61d0
DaFa <randafa@foxmail.com>
2018-01-25 14:28:56 +0800

    Update MacAddressManager.m

--- GetMacAddress/MacAddressManager.m
+++ GetMacAddress/MacAddressManager.m
@@ -130,41 +130,6 @@
     return inet_pton(AF_INET, [IPAddr UTF8String], &network) == 1 ?
     network : INADDR_NONE;
 }//end IPv4Pton:
-+ (nullable NSString *)linkLayerNtop: (nonnull struct sockaddr_dl *)sdl {
-    if(sdl->sdl_alen == 0) {
-        return nil;
-    }//end if
-    
-    NSMutableString *buf = [[NSMutableString alloc] initWithString:@""];
-    char *cp = (char *)LLADDR(sdl);
-    int n;
-    
-    if ((n = sdl->sdl_alen) > 0) {
-        while (--n >= 0) {
-            [buf appendFormat:@"%02x%s", *cp++ & 0xff, n > 0 ? ":" : ""];
-        }//end if
-    }//end if
-    
-    return [NSString stringWithString:buf];
-}//end linkLayerNtop:
-
-+ (BOOL)macAddress: (nonnull NSString *)addr1 isEqualTo:(nonnull NSString *)addr2 {
-    struct ether_addr ether_addr1, ether_addr2;
-    
-    struct ether_addr *tmp;
-    
-    tmp = ether_aton(([addr1 UTF8String]));
-    if(!tmp)
-        return NO;
-    memcpy(&ether_addr1, tmp, sizeof(ether_addr1));
-    
-    tmp = ether_aton([addr2 UTF8String]);
-    if(!tmp)
-        return NO;
-    memcpy(&ether_addr2, tmp, sizeof(ether_addr2));
-    
-    return memcmp(&ether_addr1, &ether_addr2, sizeof(struct ether_addr)) == 0 ? YES : NO;
-}//end macAddress: isEqualTo:
 
 
 @end
